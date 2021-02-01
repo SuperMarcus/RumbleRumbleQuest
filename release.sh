2634c4762e224b5388f6a4eae139d3ab8dbb15c6
Marcus Zhou <other.marcus@icloud.com>
2021-02-01 13:08:28 -0500

    Update release script

--- release.sh
+++ release.sh
@@ -2,4 +2,9 @@
 
 rm -f RumbleRumbleQuest.zip
 zip -j RumbleRumbleQuest.zip bmbfmod.json cover.gif
-find ./libs/arm64-v8a ! -name 'libmodloader.so' -type f -exec zip -j RumbleRumbleQuest.zip {} +
+find ./libs/arm64-v8a \
+  ! -name 'libmodloader.so' \
+  ! -name 'libcodegen_*.so' \
+  ! -name 'libcustom-types.so' \
+  ! -name 'libquestui.so' \
+  -type f -exec zip -j RumbleRumbleQuest.zip {} +
